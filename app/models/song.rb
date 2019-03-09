c2358d163a71446376c32a9804a153b4b9d4e26c
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-09 19:08:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,13 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
   belongs_to :artist
   belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
 end
